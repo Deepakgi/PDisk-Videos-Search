4be4c0193a94f2d796b34fe35318e8f1fd244064
Deepakgi <66556747+Deepakgi@users.noreply.github.com>
2021-12-06 14:04:27 +0530

    Update get_cookies.py

--- core/get_cookies.py
+++ core/get_cookies.py
@@ -1,23 +1,23 @@
 # (c) @AbirHasan2005
 
-from core.login import pdisk_login
+from core.login import droplink_login
 
-PDisk_DB = {}
+droplink_DB = {}
 
 
 async def get_cookies(username: str, password: str) -> str:
-    if not PDisk_DB:
-        user_id, cookies = await pdisk_login(username, password)
-        PDisk_DB["cookies"] = cookies
-        PDisk_DB["user_id"] = user_id
-        PDisk_DB["username"] = username
-        PDisk_DB["password"] = password
+    if not droplink_DB:
+        user_id, cookies = await droplink_login(username, password)
+        droplink_DB["cookies"] = cookies
+        droplink_DB["user_id"] = user_id
+        droplink_DB["username"] = username
+        droplink_DB["password"] = password
 
-    return PDisk_DB["cookies"]
+    return droplink_DB["cookies"]
 
 
 async def set_cookies(data: dict):
-    PDisk_DB["username"] = data["username"]
-    PDisk_DB["password"] = data["password"]
-    PDisk_DB["user_id"] = data["user_id"]
-    PDisk_DB["cookies"] = data["cookies"]
+    droplink_DB["username"] = data["username"]
+    droplink_DB["password"] = data["password"]
+    droplink_DB["user_id"] = data["user_id"]
+    droplink_DB["cookies"] = data["cookies"]
